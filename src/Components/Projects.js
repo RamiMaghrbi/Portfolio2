ac59a7ba885ba16906efa95d58c1b0b3fcede26c
ameen <m.ameen@canadasmm.com>
2025-01-20 13:14:25 +0300

    final

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ProjectBox from './ProjectBox';
-import shake from '../images/Screenshot 2024-10-29 195532.png';
-import laimonah from '../images/Screenshot 2024-10-29 200446.png';
-import stone from '../images/Screenshot 2024-10-29 200052.png';
 import rent from '../images/Screenshot 2024-10-29 195600.png';
 import Arab from "../images/arab-sign.jpg"
 import Booksmm from "../images/booksmm.png"
